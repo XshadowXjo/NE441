d50675764d06bbc1964b2c22511f5c3ce256a2cc
Josias <124369926+XshadowXjo@users.noreply.github.com>
2024-09-12 14:33:56 +0200

    Update ClientChenillard.java

--- tdm2/ClientChenillard.java
+++ tdm2/ClientChenillard.java
@@ -24,34 +24,35 @@ public class ClientChenillard {
         int portSrv = 3000;
         InetSocketAddress adrDest = new InetSocketAddress("127.0.0.1", portSrv);
 		
-        byte[] bufE = new String(""+portSrc).getBytes();
+        byte[] bufE = new String(""+flag).getBytes();
         DatagramPacket dpE = new DatagramPacket(bufE, bufE.length, adrDest);
         socket.send(dpE);
-    	System.out.println("Envoi d'un paquet UDP avec le n° de port : " + portSrc);
+    	System.out.println("Envoi d'un paquet UDP avec le flag : " + flag);
     	
         JFrame frame = new JFrame("Chenillard");
         frame.setSize(300,300);
                 
 		
-		for (int k=0; k<3; k++) {
+		for (int k=0; k<5; k++) {
         	frame.getContentPane().setBackground(Color.GREEN);
             frame.setVisible(true);
 	    	
-	    	// Attente de la reponse 
 	        byte[] bufR = new byte[2048];
 	        DatagramPacket dpR = new DatagramPacket(bufR, bufR.length);
 	        socket.receive(dpR);
 	        String reponse = new String(bufR, dpR.getOffset(), dpR.getLength());
 	        System.out.println("Le serveur  a répondu "+reponse); 
 	        
-	    	frame.getContentPane().setBackground(Color.RED);
-	        frame.setVisible(true);
-	        Thread.sleep(1000);
-	        
-	        byte[] bufE = new String("switch").getBytes();
-	        DatagramPacket dpE = new DatagramPacket(bufE, bufE.length, adrDest);
-	        socket.send(dpE);
-	    	System.out.println("Envoi d'un paquet UDP avec switch");
+	    	if (reponse.startsWith("red")) {
+	    		frame.getContentPane().setBackground(Color.RED);
+	    		frame.setVisible(true);
+	    		Thread.sleep(1000);
+	    	}
+	    	else if (reponse.startsWith("green")) {
+	    		frame.getContentPane().setBackground(Color.GREEN);
+	    		frame.setVisible(true);
+	    		Thread.sleep(1000);
+	    	}	        
         }
 		
 		frame.dispose();
