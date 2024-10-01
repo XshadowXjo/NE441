680300938b5116fd6063af5dac754a8c3dff6062
Josias <124369926+XshadowXjo@users.noreply.github.com>
2024-10-01 11:07:07 +0200

    Update ClientSommeTCP.java

--- tdm5/ClientSommeTCP.java
+++ tdm5/ClientSommeTCP.java
@@ -1,6 +1,8 @@
 package fr.esisar;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,48 +11,67 @@ import java.net.Socket;
 
 public class ClientSommeTCP extends Thread{
 	
-	private HashMap<Integer, Integer> MontantParPort = new HashMap<>();
 	private int port;
+	private long montant;
 	
 	public ClientSommeTCP(int port) {
 		this.port = port;
 	}
 	
-	public static void main(String[] args) throws IOException {
-		ClientSommeTCP client = new ClientSommeTCP(21000);
-		client.execute();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		List<ClientSommeTCP> clients = new ArrayList<>();
+		for(int k=21000; k<21016; k++) {
+			ClientSommeTCP client = new ClientSommeTCP(k);
+			clients.add(client);
+			client.start();
+		}
+		long montantTotal = 0;
+		long maxMontant = 0;
+		for(ClientSommeTCP c: clients) {
+			c.join();
+			if(c.montant > maxMontant)
+				maxMontant = c.montant;
+			montantTotal +=c.montant;
+		}
+		System.out.println("montant total :"+ montantTotal);
 	}
 	
-	public void execute() throws IOException {
+	public void run() {
 				
 		System.out.println("Demarrage du client ...");
 
-        //Creation de la socket
-        Socket socket = new Socket();
-
-        // Connexion au serveur
-        InetSocketAddress adrDest = new InetSocketAddress("127.0.0.1", port);
-        socket.connect(adrDest);        
-
-        // Envoi de la requete
-        byte[] bufE = new String("COMBIEN").getBytes();
-        OutputStream os = socket.getOutputStream();
-        os.write(bufE);
         
-
-        // Attente de la reponse 
-        byte[] bufR = new byte[2048];
-        InputStream is = socket.getInputStream();
-        int lenBufR = is.read(bufR);
-        if (lenBufR!=-1)
-        {
-            String reponse = new String(bufR, 0 , lenBufR );
-            int montant = Integer.parseInt(reponse.substring(reponse.indexOf("=")+1, reponse.indexOf("E")));
-            MontantParPort.put(port, montant);
-            System.out.println("Montant ajouté sur le port "+ port +" = "+montant);
+        try {
+        	//Creation de la socket
+        	Socket socket = new Socket();
+	         //Connexion au serveur
+	        InetSocketAddress adrDest = new InetSocketAddress("127.0.0.1", port);
+	        socket.connect(adrDest);        
+	
+	         //Envoi de la requete
+	        byte[] bufE = new String("COMBIEN").getBytes();
+	        OutputStream os = socket.getOutputStream();
+	        os.write(bufE);
+	        
+	
+	         //Attente de la reponse 
+	        byte[] bufR = new byte[2048];
+	        InputStream is = socket.getInputStream();
+	        int lenBufR = is.read(bufR);
+	        if (lenBufR!=-1)
+	        {
+	            String reponse = new String(bufR, 0 , lenBufR );
+	            montant = Integer.parseInt(reponse.substring(reponse.indexOf("=")+1, reponse.indexOf("E")));
+	            System.out.println("Montant ajouté sur le port "+ port +" = "+montant);
+	        }
+	         //Fermeture de la socket
+	        socket.close();
         }
-        // Fermeture de la socket
-        socket.close();
+        catch(Exception e) {
+        	e.printStackTrace();
+        }
+        
+        
     }
 }
 	
